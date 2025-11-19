8742f9eb39704d027e716ab99191d843e34f0827
LittleDo999 <jingyuandu1@gmail.com>
2025-11-19 00:48:10 +0000

    update some description

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ print (f"Max_Revenue = {bouquet1.demand * bouquet1.price}")
 
 print (f"Welcome to the Flower Shop Simulator Version {version}!\r")
 print ("How many months would you like to run the game for?")
-month = input()
-if month < = 0:
+month = int(input())
+if month <= 0:
     print ("Please enter an integer greater than 0.")
+    
+else:
+    for month in range(month):
+        print (f"\n--- Month {month+1} ---")
+        print ("Before the month starts, there are some owner actions for you to carry out. First, review the number of staff, then decide how many bouquets to sell.")
+        print ("How many florist would you like to hire?")
+        staff = input()
+        
